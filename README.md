5279da65a1ca2ecb0f6c0cace4e34e5f2807e801
alxtpl <alxtpl@gmail.com>
2024-09-12 10:13:55 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Movie Listings App
+
+## Description
+
+This project refactors a web application from using custom web components to React. It displays a collection of movies with their showtimes, screen information, seating, and more. The data is dynamically loaded from a local JSON file and rendered using React components. Bootstrap is used for styling and responsiveness.
+
+## How to Run
+
+1. Clone this repository:
+   ```bash
+   git clone https://github.com/your-username/your-repository.git
+   ```
---
+++ main.js
@@ -0,0 +1,25 @@
+function App() {
+  const [data, setData] = React.useState(null);
+  const [loaded, setLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    async function getData() {
+      const response = await fetch("./movies.json");
+      const json = await response.json();
+      setData(json);
+      setLoaded(true);
+    }
+    getData();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="col-sm">
+        {loaded &&
+          data.movies.map((movie, i) => <Movie key={i} data={movie} />)}
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
